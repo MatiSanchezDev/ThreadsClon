fb23a3060bc3cb9d9dfbeba362e1bc989c5a3386
MatiasSanchez <sanchezmatidev@gmail.com>
2024-03-26 19:22:15 -0300

    Add serverComponents

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
   typescript: {
     ignoreBuildErrors: true
   },
